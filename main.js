64ff792f2ef2b9909ea527549dac9e112b5746bd
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-05-30 18:57:48 -0300

    Feature: Create first comunication with client

--- main.js
+++ main.js
@@ -1,9 +1,60 @@
 import TelegramBot from 'node-telegram-bot-api'
 import dotenv from 'dotenv'
+import { dificuldades, categorias } from './utils.js'
 dotenv.config()
+
+let dificuldadeSelecionada
+let categoriaSelecionada
 
 const telegram = new TelegramBot(process.env.TOKEN, { polling: true })
 
 telegram.on('text', (message) => {
-  telegram.sendMessage(message.chat.id, 'Hello World, feito por Iago Rocha e Luiz Gustavo')
+  if (message.text.toLowerCase().indexOf('/start') === 0) {
+    telegram.sendMessage(message.chat.id, 'Vamos começar o jogo, selecione uma dificuldade:', {
+      reply_markup: {
+        inline_keyboard: dificuldades,
+      },
+    })
+  }
+})
+
+telegram.on('callback_query', (callbackQuery) => {
+  if (callbackQuery.data === 'facil') {
+    dificuldadeSelecionada = 'facil'
+    telegram.sendMessage(
+      callbackQuery.message.chat.id,
+      `Dificuldade selecionada: ${callbackQuery.data}, escolha a categoria:`,
+      {
+        reply_markup: {
+          inline_keyboard: categorias,
+        },
+      }
+    )
+  }
+
+  if (callbackQuery.data === 'medio') {
+    dificuldadeSelecionada = 'medio'
+    telegram.sendMessage(
+      callbackQuery.message.chat.id,
+      `Dificuldade selecionada: ${callbackQuery.data}, escolha a categoria:`,
+      {
+        reply_markup: {
+          inline_keyboard: categorias,
+        },
+      }
+    )
+  }
+
+  if (callbackQuery.data === 'dificil') {
+    dificuldadeSelecionada = 'dificil'
+    telegram.sendMessage(
+      callbackQuery.message.chat.id,
+      `Dificuldade selecionada: ${callbackQuery.data}, escolha a categoria:`,
+      {
+        reply_markup: {
+          inline_keyboard: categorias,
+        },
+      }
+    )
+  }
 })
---
+++ utils.js
@@ -0,0 +1,47 @@
+export const categorias = [
+  [
+    {
+      text: 'plantas',
+      callback_data: 'plantas',
+    },
+    {
+      text: 'carros',
+      callback_data: 'carros',
+    },
+    {
+      text: 'cores',
+      callback_data: 'cores',
+    },
+  ],
+  [
+    {
+      text: 'paises',
+      callback_data: 'paises',
+    },
+    {
+      text: 'comidas',
+      callback_data: 'comidas',
+    },
+    {
+      text: 'entretenimento',
+      callback_data: 'entretenimento',
+    },
+  ],
+]
+
+export const dificuldades = [
+  [
+    {
+      text: 'Fácil',
+      callback_data: 'facil',
+    },
+    {
+      text: 'Médio',
+      callback_data: 'medio',
+    },
+    {
+      text: 'Difícil',
+      callback_data: 'dificil',
+    },
+  ],
+]
