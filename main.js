745f10985c8832d8d0dbe0cb21fc65da424ceaf5
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-05-30 13:37:29 -0300

    Set work environment

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+import TelegramBot from 'node-telegram-bot-api'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const telegram = new TelegramBot(process.env.TOKEN, { polling: true })
+
+telegram.on('text', (message) => {
+  telegram.sendMessage(message.chat.id, 'Hello World, feito por Iago Rocha e Luiz Gustavo')
+})
