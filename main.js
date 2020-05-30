5a10de74f047a79140c2a7a01738c9af75ba01be
Luís Gustavo <lgustavogdc@gmail.com>
2020-05-30 18:33:53 -0300

    Feature: Add function hideWord

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ dotenv.config()
 const telegram = new TelegramBot(process.env.TOKEN, { polling: true })
 
 telegram.on('text', (message) => {
-  telegram.sendMessage(message.chat.id, 'Hello World, feito por Iago Rocha e Luiz Gustavo')
+  telegram.sendMessage(message.chat.id, 'Hello World, feito por Iago Rocha e Luís Gustavo')
 })
